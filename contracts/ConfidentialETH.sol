974ad2c1fcd27ca3ea63f507461e30e24fd56356
junpei99 <ozzxdn661@outlook.com>
2025-12-26 10:49:47 +0800

    feat: introduce cETH confidential token

---
+++ contracts/ConfidentialETH.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity ^0.8.27;
+
+import {ERC7984} from "@openzeppelin/confidential-contracts/token/ERC7984/ERC7984.sol";
+import {ZamaEthereumConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {FHE, euint64} from "@fhevm/solidity/lib/FHE.sol";
+
+contract ConfidentialETH is ERC7984, ZamaEthereumConfig {
+    constructor() ERC7984("cETH", "cETH", "") {}
+
+    function mint(address to, uint64 amount) public {
+        euint64 encryptedAmount = FHE.asEuint64(amount);
+        _mint(to, encryptedAmount);
+    }
+
+    function operatorTransferFrom(address from, address to, uint64 amount) external returns (euint64) {
+        require(isOperator(from, msg.sender), ERC7984UnauthorizedSpender(from, msg.sender));
+        euint64 encryptedAmount = FHE.asEuint64(amount);
+        FHE.allow(encryptedAmount, msg.sender);
+        FHE.allowTransient(encryptedAmount, msg.sender);
+        return _transfer(from, to, encryptedAmount);
+    }
+
+    function operatorTransfer(address to, uint64 amount) external returns (euint64) {
+        euint64 encryptedAmount = FHE.asEuint64(amount);
+        FHE.allow(encryptedAmount, msg.sender);
+        FHE.allowTransient(encryptedAmount, msg.sender);
+        return _transfer(msg.sender, to, encryptedAmount);
+    }
+}
